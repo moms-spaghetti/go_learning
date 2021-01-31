b026a8c5800f6972ae0566823bd25550070af86a
moms-spaghetti <williamedwards36@aol.com>
2021-01-31 03:46:30 +0000

    inital

---
+++ 05_IfElse/ifelse.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+	name := "mike"
+
+	if name == "dave" {
+		fmt.Println("name is dave")
+	} else if name == "stan" {
+		fmt.Println("name is stan")
+	} else {
+		fmt.Println("name is", name)
+	}
+}
